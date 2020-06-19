1fab2a1482a7acd2c0b3521ae5b962b28db4990c
Md. Saber Hossain <saber.hossain@monstar-lab.com.bd>
2020-06-19 09:52:01 +0600

    ...continue

--- CustomPresenterTransition/SecondaryViewController.swift
+++ CustomPresenterTransition/SecondaryViewController.swift
@@ -12,15 +12,43 @@ class SecondaryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
-        
         let rightButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(btnDismissPressed))
         self.navigationItem.rightBarButtonItem = rightButton
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        let tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        let constraints : [NSLayoutConstraint] = [
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
     }
     
     @objc func btnDismissPressed() {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension SecondaryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
 }
